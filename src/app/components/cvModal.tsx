ebe52dd9ec9e03eafcacbbd8b02f75ada0d796fe
YannVerd <yann.e.verdier@gmail.com>
2024-09-30 09:45:25 +0000

    fix cv modal responsiv

--- src/app/components/cvModal.tsx
+++ src/app/components/cvModal.tsx
@@ -2,8 +2,8 @@ import { IModal } from "./legalNotices";
 
 export default function CVModal(props: IModal){
     return (
-        <div className="bg-white w-[80%] md:w-fit h-fit md:h-[70%] fixed rounded-lg border-secondary border-2 dark:bg-black shadow-lg left-0 lg:left-[10%] top-[8%] flex-col justify-around items-center z-50" style={{ display: props.isVisible ? 'flex' : 'none'}}>
-            <iframe src={"/pdf/cv.pdf#navpanes=0&toolbar=1&view=fit&zoom=300"}  height={800} width={600}/>
+        <div className="bg-white w-[95%] h-[90%] fixed rounded-lg border-secondary border-2 dark:bg-black shadow-lg left-2 lg:left-[3%] top-[6%] flex-col justify-around items-center z-50" style={{ display: props.isVisible ? 'flex' : 'none'}}>
+            <object data="/pdf/cv.pdf" type="application/pdf" width="100%" height="100%"></object>
             <button className="bg-secondary hover:bg-secondaryLight text-white font-bold py-2 px-4 mb-2 border mt-1 border-secondary rounded" onClick={()=> { props.hook('CV')}}>close</button>
         </div>
         
