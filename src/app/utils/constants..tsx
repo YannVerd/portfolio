6ad209921a0c5e7426ade6df19a2d0a217642185
YannVerd <yann.e.verdier@gmail.com>
2024-10-02 09:46:02 +0000

    create constants for datas and add section 'about me'

---
+++ src/app/utils/constants..tsx
@@ -0,0 +1,24 @@
+// skills's names match pictures's names
+export const skills = ['React','Expo', 'Nodejs', 'Mongoose', 'MongoDB', 'Flutter', 'MySQL', 'JavaScript', 'cpp'];
+export const projects = [
+    {
+    name: 'WitcherJDR', 
+    description: `Projet pour une feuille de Personnage automatique pour le jeu de role The witcher.
+                    Framework non possible car doit être utilisé hors ligne sur public non averti (Pas de commandes ou manipulation complexe).
+                    Server et base de donnée en serveur privé (physique local) uniquement pour sauvegarder les personnages.
+                    `,
+    link: "https://github.com/YannVerd/Witcher-CharacterSheet"
+    },
+    {
+    name: 'CPPFW',
+    description: `Il s'agit d'un framework réalisé en C++ pour créer des sites statiques en html/css `,
+    link: "https://github.com/YannVerd/CPPFW"
+    } 
+]
+export const links = [
+    {name: "GitHub", img:"/github-50.png", imgWhite:"/github-white-50.png", link: "https://github.com/YannVerd"}, 
+    {name: "LinkedIn", img:"/linkedin-50.png",imgWhite:"/linkedin-white-50.png", link: "https://www.linkedin.com/in/yann-verdier-a7b457271/"}, 
+    {name: "CV", img:"/cv-50.png", imgWhite:"/cv-white-50.png", link: "#"}
+]
+
+    
