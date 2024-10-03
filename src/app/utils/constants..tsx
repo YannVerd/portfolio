3eb337ad89c1192e5e5e6b01a24d6c7220558cac
YannVerd <yann.e.verdier@gmail.com>
2024-10-03 18:08:31 +0000

    new card and add project

--- src/app/utils/constants..tsx
+++ src/app/utils/constants..tsx
@@ -2,17 +2,22 @@
 export const skills = ['React','Expo', 'Nodejs', 'Mongoose', 'MongoDB', 'Flutter', 'MySQL', 'JavaScript', 'cpp'];
 export const projects = [
     {
-    name: 'WitcherJDR', 
-    description: `Projet pour une feuille de Personnage automatique pour le jeu de role The witcher.
-                    Framework non possible car doit être utilisé hors ligne sur public non averti (Pas de commandes ou manipulation complexe).
-                    Server et base de donnée en serveur privé (physique local) uniquement pour sauvegarder les personnages.
-                    `,
-    link: "https://github.com/YannVerd/Witcher-CharacterSheet"
+        name: 'WitcherJDR', 
+        description: `Projet de feuille de Personnage automatique pour le jeu de role The witcher.
+                        Framework non possible car il devait être utilisé hors ligne avec un public non averti (Pas de commandes ou manipulation complexe).
+                        Serveur et base de donnée en serveur privé (physique local) uniquement pour sauvegarder les personnages.
+                        `,
+        link: "https://github.com/YannVerd/Witcher-CharacterSheet"
     },
     {
-    name: 'CPPFW',
-    description: `Il s'agit d'un framework réalisé en C++ pour créer des sites statiques en html/css `,
-    link: "https://github.com/YannVerd/CPPFW"
+        name: 'CPPFW',
+        description: `Il s'agit d'un framework réalisé en C++ pour créer des sites statiques en html/css `,
+        link: "https://github.com/YannVerd/CPPFW"
+        },
+    {
+        name: "pizza_app",
+        description:"Projet d'école pour une application de livraison de pizza en flutter",
+        link:'https://github.com/YannVerd/pizza_app'
     } 
 ]
 export const links = [
