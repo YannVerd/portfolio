d67380ffb22c5a002252b150e916701b4ba9d9ec
YannVerd <yann.e.verdier@gmail.com>
2024-10-08 16:04:27 +0000

    fix warning Image / rename skill card file and component

--- src/app/components/skillCard.tsx
+++ src/app/components/skillCard.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 
-interface ISkill{
+interface ISkillCard{
     name: string,
 }
 
-export default function Skill(props : ISkill){
+export default function SkillCard(props : ISkillCard){
     return (
             <div className="flex flex-col w-16 items-center">
-                <Image src={`/skills/${props.name.toLowerCase()}.png`} width={150} height={150} alt={`${props.name}`}/>
+                <Image src={`/skills/${props.name.toLowerCase()}.png`} width={150} height={150} alt={`${props.name}`} className="w-auto h-14"/>
                 <h4 className="font-semibold md:text-xl">{props.name}</h4>
             </div>
     );
